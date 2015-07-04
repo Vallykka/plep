dcdaa4afaacf97a47f1756cf3938d9b6cc8df024
Miron Aseev <aseev.miron@gmail.com>
2015-07-04 22:00:37 +0600

    Set up the project structure

---
+++ src/main/java/com/plep/PlepApplication.java
@@ -0,0 +1,12 @@
+package com.plep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlepApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlepApplication.class, args);
+    }
+}
